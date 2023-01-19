1dff58b5302eec7767d16344b7dd941a5ff34e61
bharathip <bharathip@VMT14599.VALUEMOMENTUM.BIZ>
2023-01-19 17:10:26 +0530

    Second commit

---
+++ Bharathitask/src/Assesment/Firsttask.java
@@ -0,0 +1,35 @@
+package Assesment;
+import java.util.*;
+
+public class Firsttask {
+
+	public static void main(String[] args) {
+		
+		        Scanner sc=new Scanner(System.in);
+		        System.out.println("enter any number");
+		        int num=sc.nextInt();
+		        boolean isPrime = true;
+		            for(int i=2;i<num;i++) {  
+		            if(num%i==0) {
+		                isPrime=false;
+		                break;
+		            }
+		        }
+		        if(isPrime)
+		            System.out.println("prime");
+		        else 
+		            System.out.println("not a prime");
+
+
+
+		 
+
+		    }
+
+		 
+
+		
+
+	}
+
+
