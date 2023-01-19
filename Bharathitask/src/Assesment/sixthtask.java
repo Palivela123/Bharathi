293ee97a89f75eff2e70015651c1c1c27b39158d
bharathip <bharathip@VMT14599.VALUEMOMENTUM.BIZ>
2023-01-19 17:23:00 +0530

    7 commit

---
+++ Bharathitask/src/Assesment/sixthtask.java
@@ -0,0 +1,28 @@
+package Assesment;
+import java.util.*;
+
+public class sixthtask {
+
+	public static void main(String[] args) {
+		
+		
+		
+		        int total=0;
+		        Scanner sc=new Scanner(System.in);
+		        System.out.println("no of elem");
+		        int n=sc.nextInt();
+		        int array[]=new int[8];
+		        System.out.println("enter elem of array");
+		        for(int i=0;i<n;i++) {
+		            array[i]=sc.nextInt();    
+		        }
+		        for(int j: array)
+		            total=total+j;
+		        System.out.println(total);
+		    }
+
+		 
+
+		}
+
+	
