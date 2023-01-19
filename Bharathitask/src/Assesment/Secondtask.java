7b5a53f7b47dc2da9a2f67274bd8e251b60a2faa
bharathip <bharathip@VMT14599.VALUEMOMENTUM.BIZ>
2023-01-19 17:13:52 +0530

    3 commit

---
+++ Bharathitask/src/Assesment/Secondtask.java
@@ -0,0 +1,28 @@
+package Assesment;
+import java.util.*;
+
+public class Secondtask {
+
+	public static void main(String[] args) {
+		
+		
+		        Scanner sc=new Scanner(System.in);
+		        System.out.println("enter a string");
+		        String str=sc.nextLine();
+
+		        str=str.replaceAll("\s", "");
+		        System.out.println(str);
+
+		 
+
+		        
+
+
+
+		    }
+
+		 
+
+		}
+
+	
